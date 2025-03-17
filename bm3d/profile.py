377d1a44e8d81815fccd609e51fdb311b54e79c7
IBIBENDUM <ibibendoom@gmail.com>
2025-03-17 05:32:18 +0300

    refactor: change filter threshold type to float

--- bm3d/profile.py
+++ bm3d/profile.py
@@ -10,22 +10,52 @@ class PositiveIntDescriptor:
         return getattr(obj, f"_{self.name}")
 
     def __set__(self, obj, value):
+        if not isinstance(value, int):
+            raise ValueError(f"{self.name} must be an integer")
         if value <= 0:
             raise ValueError(f"{self.name} must be a positive integer")
         setattr(obj, f"_{self.name}", value)
 
+class PositiveFloatDescriptor:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, obj, objtype):
+        return getattr(obj, f"_{self.name}")
+
+    def __set__(self, obj, value):
+        if not isinstance(value, float):
+            raise ValueError(f"{self.name} must be an float")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be a positive float")
+        setattr(obj, f"_{self.name}", value)
+
 # TODO: Add profiles fast and normal
 class BM3DProfile:
-    __slots__ = ("_blockSize", "_distanceThreshold", "_filterThreshold", "_groupMaxSize")
+    __slots__ = (
+        "_blockSize",
+        "_blockStep",
+        "_distanceThreshold",
+        "_filterThreshold",
+        "_groupMaxSize",
+    )
 
     blockSize = PositiveIntDescriptor("blockSize")
-    groupMaxSize = PositiveIntDescriptor("groupMaxSize")
+    blockStep = PositiveIntDescriptor("blockStep")
     distanceThreshold = PositiveIntDescriptor("distanceThreshold")
-    filterThreshold = PositiveIntDescriptor("filterThreshold")
+    filterThreshold = PositiveFloatDescriptor("filterThreshold")
+    groupMaxSize = PositiveIntDescriptor("groupMaxSize")
 
-    def __init__(self, blockSize: int=16, distanceThreshold: int=500, filterThreshold: float=10.00,
-                 groupMaxSize: int = 16):
+    def __init__(
+        self,
+        blockSize: int = 16,
+        blockStep: int = 16,
+        distanceThreshold: int = 100,
+        filterThreshold: float = 10.00,
+        groupMaxSize: int = 16,
+    ):
         self._blockSize = blockSize
+        self._blockStep = blockStep
         self._groupMaxSize = groupMaxSize
         self._distanceThreshold = distanceThreshold
         self._filterThreshold = filterThreshold
