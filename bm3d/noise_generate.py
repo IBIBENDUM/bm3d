75bcd4cfe3ca60bb4ffaaf58cb849ad7cf09013a
IBIBENDUM <ibibendoom@gmail.com>
2025-03-21 05:40:52 +0300

    fix: remove random.seed from addNoise()

--- bm3d/noise_generate.py
+++ bm3d/noise_generate.py
@@ -9,7 +9,6 @@ def addNoise(image: np.ndarray, noiseVariance: float) -> np.ndarray:
     Return:
         Image with noise
     """
-    np.random.seed(0)
     gaussianNoise = np.random.normal(0, noiseVariance, image.shape)
     gaussianNoise = gaussianNoise.reshape(image.shape)
     noisyImage = image + gaussianNoise
