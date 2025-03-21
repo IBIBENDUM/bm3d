b930e2d2c900a6ce25681592b33c5a5e6f891a08
IBIBENDUM <ibibendoom@gmail.com>
2025-03-21 16:01:58 +0300

    feature: update package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BM3D",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.21.0",
