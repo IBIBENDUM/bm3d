16aae303127e3a42937bcdfb0dfb6b7c58274d63
IBIBENDUM <ibibendoom@gmail.com>
2025-04-03 19:22:09 +0300

    fix: optuna checkpoints

--- unet/optuna_trainer.py
+++ unet/optuna_trainer.py
@@ -1,7 +1,7 @@
-import optuna
-import os
+import datetime
 from pathlib import Path
 import pandas as pd
+import optuna
 import optuna.visualization as vis
 from train import ModelTrainer
 
@@ -9,18 +9,41 @@ class OptunaModelTrainer(ModelTrainer):
     def __init__(
         self,
         studyName="optuna_study",
-        storagePath="sqlite:///optuna_study.db",
         saveInterval=5,
         epochs=30,
+        loadDB=None
     ):
         super().__init__()
         self.saveInterval = saveInterval
         self.config.train["epochs"] = epochs
-        self.study = optuna.create_study(
-            study_name=studyName,
+
+        self.storagePath = self.getStoragePath(studyName, loadDB)
+        if loadDB:
+            self.study = self.loadExistingStudy(studyName, self.storagePath)
+        else:
+            self.study = self.createNewStudy(studyName, self.storagePath)
+
+    def getStoragePath(self, studyName, loadDB):
+        dbDir = Path("optuna_results/db")
+        dbDir.mkdir(parents=True, exist_ok=True)
+
+        if loadDB:
+            self.logger.info(f"Loading existing study from {loadDB}")
+            return f"sqlite:///{loadDB}"
+        else:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            return f"sqlite:///{dbDir}/{studyName}_{timestamp}.db"
+
+    def loadExistingStudy(self, studyName, storagePath):
+        return optuna.load_study(study_name=studyName, storage=storagePath)
+
+    def createNewStudy(self, studyName, storagePath):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        return optuna.create_study(
+            study_name=f"{studyName}_{timestamp}",
             storage=storagePath,
             direction="minimize",
-            load_if_exists=True,
+            load_if_exists=False,
         )
 
     def suggestHyperparameters(self, trial):
@@ -84,7 +107,7 @@ class OptunaModelTrainer(ModelTrainer):
             f'{self.metrics["val"]["loss"][-1]:.4f}'
         )
 
-        self.study.storage.commit()
+        # self.study.storage.commit()
         # Save results every `saveInterval` epochs
         if (epoch + 1) % self.saveInterval == 0:
             self.evaluateModel(epoch)
@@ -121,7 +144,7 @@ class OptunaModelTrainer(ModelTrainer):
         
         # Save the final model weights at the end of the trial
         self.saveResults()
-        self.study.storage.commit()
+        # self.study.storage.commit()
         return finalLoss
 
     def optimize(self, nTrials=10):
@@ -140,32 +163,32 @@ class OptunaModelTrainer(ModelTrainer):
 
     def saveOptimizationResults(self, study):
         """Save optimization results and plots"""
-        outputDir = "optuna_results"
-        os.makedirs(outputDir, exist_ok=True)
+        outputDir = Path("optuna_results")
+        outputDir.mkdir(parents=True, exist_ok=True)
         self.logger.info(f"Output directory created: {outputDir}")
 
         historyDf = pd.DataFrame(
             [(trial.number + 1, trial.value) for trial in study.trials],
             columns=["Trial", "Objective"],
         )
-        historyCsvPath = os.path.join(outputDir, "optimization_history.csv")
+        historyCsvPath = outputDir / "optimization_history.csv"
         historyDf.to_csv(historyCsvPath, index=False)
         self.logger.info(f"Optimization history saved to {historyCsvPath}.")
 
         # Plot optimization history using Optuna's visualization library
         fig = vis.plot_optimization_history(study)
-        historyImgPath = os.path.join(outputDir, "optimization_history.png")
+        historyImgPath = outputDir / "optimization_history.png"
         fig.write_image(historyImgPath)
         self.logger.info(f"Optimization history plot saved to {historyImgPath}.")
 
         # Plot parameter importance
         fig2 = vis.plot_param_importances(study)
-        importanceImgPath = os.path.join(outputDir, "param_importance.png")
+        importanceImgPath = outputDir / "param_importance.png"
         fig2.write_image(importanceImgPath)
         self.logger.info(f"Parameter importance plot saved to {importanceImgPath}.")
 
 if __name__ == "__main__":
     optunaTrainer = OptunaModelTrainer(
-        saveInterval=5, epochs=35, enableCheckpoints=True
+        saveInterval=5, epochs=35
     )
     optunaTrainer.optimize(nTrials=30)
