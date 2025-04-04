096aa3c9b84aac37d4480e0f9786508ae03b6567
IBIBENDUM <ibibendoom@gmail.com>
2025-04-04 20:33:33 +0300

    refactor: test.py

--- unet/test.py
+++ unet/test.py
@@ -1,36 +1,76 @@
 import torch
 from model import UNet
-from torchvision.transforms import ToTensor
 from PIL import Image
 import numpy as np
+from config_manager import Config
+from dataloader import getDataLoader
+from tqdm import tqdm
+import piq
+import csv
+from pathlib import Path
 
 def loadModel(modelPath, device):
     model = UNet().to(device)
     model.load_state_dict(torch.load(modelPath, map_location=device))
     model.eval()
-
     return model
 
-def denoiseImage(model, imagePath, device):
-    image = Image.open(imagePath).convert("L")
-    inputTensor = ToTensor()(image).unsqueeze(0).to(device)
+def calculateImageMetrics(output, clean):
+    """Calculate metrics for single image"""
+    return {
+        "psnr": piq.psnr(output, clean).item(),
+        "ssim": piq.ssim(output, clean).item(),
+        "vif": piq.vif_p(output, clean).item(),
+    }
 
-    with torch.no_grad():
-        outputTensor = model(inputTensor)
+def saveDenoisedImage(outputImg, imageIndex, outputDir):
+    outputImagePath = outputDir / f"denoised_{imageIndex}.png"
+    denoisedImage = outputImg.squeeze(0).cpu().numpy()
+    denoisedImage = np.clip(denoisedImage, 0, 1) * 255
+    denoisedImage = denoisedImage.astype(np.uint8)
+    denoisedImage = Image.fromarray(denoisedImage[0])
 
-    outputTensor = outputTensor.squeeze(0).cpu().numpy() 
-    outputTensor = np.clip(outputTensor, 0, 1) 
-    outputTensor = (outputTensor * 255).astype('uint8')
-    denoisedImage = Image.fromarray(outputTensor.squeeze()) 
-    
-    return denoisedImage
+    denoisedImage.save(outputImagePath)
+
+def saveMetricsToCsv(metrics, outputCsv):
+    metricsNames = ["imageIndex", "psnr", "ssim", "vif"]
+    with open(outputCsv, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(metricsNames)
+        writer.writerows(metrics) 
 
 if __name__ == "__main__":
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    config = Config()
+    model = loadModel("weights.pth", config.train["device"])
+    dataLoader = getDataLoader(
+        sourceDir=config.paths["cleanValDir"],
+        batchSize=config.train["batchSize"],
+        numWorkers=config.train["numWorkers"],
+        augment=False,
+        shuffle=False,
+    )
 
-    model = loadModel("weights/weights1.pth", device)
+    results = []
 
-    noisyImagePath = "test_images/cameraman256.jpg"
-    denoisedImage = denoiseImage(model, noisyImagePath, device)
-    denoisedImage.save("denoised_image.png")
+    outputDir = Path("test_output")
+    outputDir.mkdir(parents=True, exist_ok=True)
+    with torch.inference_mode():
+        for batchIndex, (noisy, clean) in tqdm(enumerate(dataLoader), desc="Evaluating data"):
+            noisy = noisy.to(config.train["device"])
+            clean = clean.to(config.train["device"])
+            outputs = model(noisy)
 
+            for i in range(noisy.size(0)):
+                noisyImg = noisy[i].unsqueeze(0)
+                cleanImg = clean[i].unsqueeze(0)
+                outputImg = outputs[i].unsqueeze(0)
+
+                batchMetrics = calculateImageMetrics(outputImg, cleanImg)
+
+                imageIndex = batchIndex * config.train["batchSize"] + i
+
+                results.append([imageIndex] + list(batchMetrics.values()))
+
+                saveDenoisedImage(outputImg, imageIndex, outputDir)
+
+    saveMetricsToCsv(results, "metrics.csv")
